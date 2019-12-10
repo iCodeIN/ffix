d24af959e4ab4952640105947acdac6a5c5f4766
Ross Nomann <rossnomann@protonmail.com>
2019-12-11 01:50:19 +0400

    Impl Display and Error for Error

--- src/error.rs
+++ src/error.rs
@@ -1,15 +1,21 @@
 use std::{
+    error::Error as StdError,
     ffi::{IntoStringError, NulError},
+    fmt,
     result::Result as StdResult,
     str::Utf8Error,
 };
 
-// TODO: impl display and std error
+/// Describes all errors that may occur
 #[derive(Debug)]
 pub enum Error {
+    /// Error when converting a CString into a String
     IntoString(IntoStringError),
+    /// Interior nul byte was found
     NulByte(NulError),
+    /// Got a NULL pointer
     Null,
+    /// Can not create UTF-8 string
     Utf8(Utf8Error),
 }
 
@@ -31,4 +37,29 @@ impl From<Utf8Error> for Error {
     }
 }
 
+impl fmt::Display for Error {
+    fn fmt(&self, out: &mut fmt::Formatter) -> fmt::Result {
+        use self::Error::*;
+        match self {
+            IntoString(ref err) => write!(out, "string conversion error: {}", err),
+            NulByte(ref err) => write!(out, "nul byte error: {}", err),
+            Null => write!(out, "got a NULL pointer"),
+            Utf8(ref err) => write!(out, "UTF-8 error: {}", err),
+        }
+    }
+}
+
+impl StdError for Error {
+    fn source(&self) -> Option<&(dyn StdError + 'static)> {
+        use self::Error::*;
+        Some(match self {
+            IntoString(ref err) => err,
+            NulByte(ref err) => err,
+            Utf8(ref err) => err,
+            _ => return None,
+        })
+    }
+}
+
+/// A specialized result type for FFI utilities
 pub type Result<T> = StdResult<T, Error>;
