24e1c838e0e7a556655557f8dd791e67554ec22a
Ross Nomann <rossnomann@protonmail.com>
2019-12-11 01:53:37 +0400

    Docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,12 @@
+//! Rust FFI utilities
+#![warn(missing_docs)]
+
 mod error;
 
+/// Array-related utilities
 pub mod array;
+
+/// String-related utilities
 pub mod string;
 
 pub use self::error::{Error, Result};
