3ace8be5b559f8feffbc469cf2c87605527ee1ef
Ross Nomann <rossnomann@protonmail.com>
2019-12-11 01:51:09 +0400

    Add ArrayReaderIter and tests for ArrayReader

--- src/array.rs
+++ src/array.rs
@@ -1,38 +1,140 @@
-use crate::{Error, Result};
-
+/// Null-terminated C array reader
 pub struct ArrayReader<T> {
     ptr: *mut *mut T,
-    curr_idx: isize,
-    max_len: isize,
 }
 
 impl<T> ArrayReader<T> {
-    pub fn new(ptr: *mut *mut T, max_len: isize) -> Result<Self> {
+    /// Create a new array from raw pointer
+    ///
+    /// # Panics
+    ///
+    /// Panics if pointer is NULL
+    ///
+    /// # Arguments
+    ///
+    /// * ptr - Pointer to array
+    pub unsafe fn new(ptr: *mut *mut T) -> Self {
+        assert!(!ptr.is_null());
+        Self { ptr }
+    }
+
+    /// Get an item from array by index
+    ///
+    /// # Panics
+    ///
+    /// Panics if array pointer is NULL
+    ///
+    /// # Arguments
+    ///
+    /// * index - Index of an item
+    pub fn get(&self, index: usize) -> Option<*mut T> {
+        assert!(!self.ptr.is_null());
+        let ptr = unsafe { *self.ptr.offset(index as isize) };
         if ptr.is_null() {
-            Err(Error::Null)
+            None
         } else {
-            Ok(Self {
-                ptr,
-                max_len,
-                curr_idx: 0,
-            })
+            Some(ptr)
         }
     }
 }
 
-impl<T> Iterator for ArrayReader<T> {
+impl<T> IntoIterator for ArrayReader<T> {
+    type Item = *mut T;
+    type IntoIter = ArrayIter<T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        ArrayIter::new(self)
+    }
+}
+
+/// Iterator over a C array reader
+pub struct ArrayIter<T> {
+    reader: ArrayReader<T>,
+    current_index: usize,
+}
+
+impl<T> ArrayIter<T> {
+    fn new(reader: ArrayReader<T>) -> Self {
+        Self {
+            reader,
+            current_index: 0,
+        }
+    }
+}
+
+impl<T> Iterator for ArrayIter<T> {
     type Item = *mut T;
 
     fn next(&mut self) -> Option<Self::Item> {
-        if self.ptr.is_null() || self.curr_idx == self.max_len {
+        if self.reader.ptr.is_null() {
             return None;
         }
-        let item_ptr = unsafe { *self.ptr.offset(self.curr_idx) };
-        if item_ptr.is_null() {
-            None
-        } else {
-            self.curr_idx += 1;
-            Some(item_ptr)
+        let item = self.reader.get(self.current_index);
+        self.current_index += 1;
+        item
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use libc::{free, malloc};
+    use std::{mem, ptr::null_mut};
+
+    #[repr(C)]
+    struct Item {
+        value: usize,
+    }
+
+    struct Array {
+        ptr: *mut *mut Item,
+    }
+
+    impl Array {
+        fn new(length: usize) -> Self {
+            let item_size = mem::size_of::<*const Item>();
+            let array_size = item_size * (length + 1);
+            let array_ptr = unsafe {
+                let ptr = malloc(array_size);
+                assert!(!ptr.is_null());
+                ptr as *mut *mut Item
+            };
+            for i in 0..length {
+                unsafe {
+                    let ptr = malloc(item_size);
+                    assert!(!ptr.is_null());
+                    let ptr = ptr.cast::<Item>();
+                    (*ptr).value = i;
+                    *array_ptr.offset(i as isize) = ptr;
+                };
+            }
+            Self { ptr: array_ptr }
         }
+    }
+
+    impl Drop for Array {
+        fn drop(&mut self) {
+            unsafe { free(self.ptr.cast()) }
+        }
+    }
+
+    #[test]
+    fn it_works() {
+        let length = 10;
+        let array = Array::new(length);
+        let reader = unsafe { ArrayReader::new(array.ptr) };
+        for i in 0..length {
+            let item = reader.get(i).unwrap();
+            assert_eq!(unsafe { (*item).value }, i)
+        }
+        assert!(reader.get(length * 10).is_none());
+        let values: Vec<usize> = reader.into_iter().map(|x| unsafe { (*x).value }).collect();
+        assert_eq!(values, vec![0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    }
+
+    #[test]
+    #[should_panic]
+    fn create_from_null() {
+        let _: ArrayReader<Item> = unsafe { ArrayReader::new(null_mut()) };
     }
 }
